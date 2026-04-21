44e4acb819545263666585a602d76b520a2b93b1
bistpramod <bistpramod113@gmail.com>
2026-04-21 14:50:01 +0545

    ready to edit the files

--- src/config/Router.tsx
+++ src/config/Router.tsx
@@ -3,8 +3,15 @@ import NotFound from "../pages/error/NotFound";
 import { AdminRouter } from "../lib/router/admin-router";
 import { UserRouter } from "../lib/router/user-router";
 import { PublicRouter } from "../lib/router/public-router";
+import ProductLayout from "../pages/layouts/ProductLayout";
+import ProductDetail from "../pages/product/ProductDetail";
+import AllProductList from "../pages/product/AllProductList";
 
 const routerData = createBrowserRouter([
+  {path:"/product-list", element: <ProductLayout/>, children:[
+    {index:true, element: <AllProductList/>},  // index route 
+    {index:false, path:":id", element: <ProductDetail/>}
+  ]},
   ...PublicRouter,
   ...AdminRouter,
   ...UserRouter,
